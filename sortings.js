528db66f1ffa6c5b986ec77f2eca197bd5f8e6e5
kronung <volokh.dmitry@tut.by>
2015-01-01 22:55:20 +0300

    sortings initial commit

---
+++ sortings.js
@@ -0,0 +1,38 @@
+(function main() {
+	var inputArray = [44, 55, 12, 42, 94, 18, 06, 67];
+
+	function insertionSort(input) {
+		for (var i = 0; i < input.length - 1; i++) {
+			var x = input[i],
+				j = i;
+			while ((j > 0) && (x < input[j-1])) {
+				input[j] = input[j - 1];
+				j--;
+			}
+			input[j] = x;
+		};
+		return input;
+	};
+
+	function selectionSort(input) {
+		var l = input.length;
+		for (var i = 0; i < l - 2; i++) {
+			var x = input[i],
+				k = i;
+			for (var j = i + 1; j < l - 1; j++) {
+				if (input[j] < x) {
+					k = j;
+					x = input[k];
+				}
+			};
+			input[k] = input[i];
+			input[i] = x;
+		};
+		return input;
+	};
+
+	
+
+	console.log(insertionSort(inputArray));
+	console.log(selectionSort(inputArray));
+})();
